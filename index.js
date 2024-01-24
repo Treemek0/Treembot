3d4458873de408e30ae811381eca6fcf17ecc3b8
Treemek <67553502+Treemek0@users.noreply.github.com>
2024-01-24 21:27:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// const { clientId, guildId, token, publicKey } = require('./config.json');
+require('dotenv').config()
+const APPLICATION_ID = process.env.APPLICATION_ID 
+const TOKEN = process.env.TOKEN 
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
+const GUILD_ID = process.env.GUILD_ID 
 
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const express = require('express');
+const app = express();
+app.get('/', async (req,res) =>{
+  return res.send('None')
+})
+
+app.listen(8999, () => {
+})
+
+// Log in to Discord with your client's token
+client.login(TOKEN);
